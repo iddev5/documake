f38a0d78193dd0c08892f348ac3f8e6442597032
iddev5 <ayushtechportal@gmail.com>
2021-03-20 00:00:54 +0530

    CSV Table column size is now calculated Note: still not good result, might have to switch to a better table style

--- documake.py
+++ documake.py
@@ -10,10 +10,14 @@ def getprop(doc, name):
 
 def parsemember(doc, name):
     if name == "struct" or name == "func":
-        out = "   {}, {}\n".format(getprop(doc, "type"), getprop(doc, "name"));
+        prop1 = getprop(doc, "type");
+        prop2 = getprop(doc, "name");
+        out  = "   {}, {}\n".format(prop1, prop2);
     else:
-        out = "   {}, {}\n".format(getprop(doc, "name"), getprop(doc, "value"));
-    return out;
+        prop1 = getprop(doc, "name");
+        prop2 = getprop(doc, "value");
+        out = "   {}, {}\n".format(prop1, prop2);
+    return out, len(prop1), len(prop2) if type(prop2) is not int else 1;
 
 def decltype(name):
     decls = {"func": "Parameter", "struct": "Member", "enum": "Enum"}
@@ -25,10 +29,17 @@ def parsedecl(doc, name):
     out += "Description\n{}\n{}\n".format("-" * len("Description"), getprop(doc, "desc"));
     out += "{}s\n{}\n\n".format(decltype(name), "-" * (len(decltype(name)) + 1));
     out += ".. csv-table::\n";
-    out += "   :widths: 15, 15\n\n";
+    out += "   :widths: $$width$$, $$height$$\n\n";
     
+    lenmax1 = lenmax2 = 0;
     for _, comp in getprop(doc, "comp").items():
-        out += parsemember(comp, name);
+        (outstr, len1, len2) = parsemember(comp, name);
+        out += outstr;
+        lenmax1 = max(len1, lenmax1);
+        lenmax2 = max(len2, lenmax2);
+
+    out = out.replace("$$width$$", str(lenmax1), 1);
+    out = out.replace("$$height$$", str(lenmax2), 1);
     
     out += "\n\n----\n\n";
     return out;
